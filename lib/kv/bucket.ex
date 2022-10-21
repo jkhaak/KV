c5d844b7c29617076399b019b0b5b316f074523d
Jani Haakana <jani.haakana@icloud.com>
2022-10-21 23:35:51 +0300

    Add some remarks regarding Agent usage

--- lib/kv/bucket.ex
+++ lib/kv/bucket.ex
@@ -28,6 +28,13 @@ defmodule KV.Bucket do
   Returns the current value of `key`, if `key` exists.
   """
   def delete(bucket, key) do
-    Agent.get_and_update(bucket, &Map.pop(&1, key))
+    # puts client to sleep
+    Process.sleep(1000)
+
+    Agent.get_and_update(bucket, fn dict ->
+      # puts server to sleep
+      Process.sleep(1000)
+      Map.pop(dict, key)
+    end)
   end
 end
